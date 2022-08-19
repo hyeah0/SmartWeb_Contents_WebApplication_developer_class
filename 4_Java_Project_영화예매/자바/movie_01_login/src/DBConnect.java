7922da59bcc372274407b94ebe0cabcf18498a8a
hye <hye0ah0@gmail.com>
2022-08-19 12:18:19 +0900

    Update DBConnect.java

--- 4_Java_Project_영화예매/자바/movie_01_login/src/DBConnect.java
+++ 4_Java_Project_영화예매/자바/movie_01_login/src/DBConnect.java
@@ -6,12 +6,12 @@ import java.sql.DriverManager;
 
 /*
  * 
- * 클래스명 : Movie_System 
- * Movie_System.getConnection();
+ * 클래스명 : DBConnect 
+ * DBConnect.getConnection();
  *
  */
 
-public class Movie_System {
+public class DBConnect {
 
 
 	public static Connection getConnection() {
@@ -21,14 +21,14 @@ public class Movie_System {
 		// 윈도우용 
 			// String driver = "oracle.jdbc.driver.OracleDriver";	
 			// String url = "jdbc:oracle:thin@localhost:1521:xe";
-			// String user = "system1";
-			// String pwd = "1234";
+			// String user = "사용자이름";
+			// String pwd = "비밀번호";
 		
 		// 오라클 개인 주소값(맥)
 			String driver = "oracle.jdbc.driver.OracleDriver";
-			String url = "jdbc:oracle:thin:@SYSTEM_high?TNS_ADMIN=/Users/hye/oraclewallet/Wallet_system";
-			String userid = "admin";
-			String pwd = "Asdfghjklzxc0";
+			String url = "jdbc:oracle:thin:@전자지갑이름_high?TNS_ADMIN=전자지갑경로\";
+			String user = "사용자이름";
+		    String pwd = "비밀번호";
 	
 		// 오라클 드라이버 로딩 및 데이터 베이스 연결 
 		try {
@@ -36,7 +36,7 @@ public class Movie_System {
 			Class.forName(driver);
 			
 			/* 2단계 : 데이터베이스 연결 및 준비 */
- 	        con = DriverManager.getConnection(url, userid, pwd); 
+ 	        con = DriverManager.getConnection(url, user, pwd); 
  	        // System.out.println("오라클 로딩, 데이터베이스 연결 완료!");
  	        
 			
