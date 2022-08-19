c622689dd7da214228daea1e6eaf26e14203d452
hye <hye0ah0@gmail.com>
2022-08-19 11:42:36 +0900

    Create LoginMember_DTO.java

---
+++ 4_Java_Project_영화예매/자바/movie_01_login/src/LoginMember_DTO.java
@@ -0,0 +1,44 @@
+package movie_01_login;
+
+public class LoginMember_DTO {
+
+	// 변수
+	private static String id;
+	private static String memberName;
+	private static String cardNum;
+	private static int cardPwd;
+
+	// getter/setter
+	public static String getId() {
+		return id;
+	}
+
+	public static void setId(String id) {
+		LoginMember_DTO.id = id;
+	}
+
+	public static String getMemberName() {
+		return memberName;
+	}
+
+	public static void setMemberName(String memberName) {
+		LoginMember_DTO.memberName = memberName;
+	}
+
+	public static String getCardNum() {
+		return cardNum;
+	}
+
+	public static void setCardNum(String cardNum) {
+		LoginMember_DTO.cardNum = cardNum;
+	}
+
+	public static int getCardPwd() {
+		return cardPwd;
+	}
+
+	public static void setCardPwd(int cardPwd) {
+		LoginMember_DTO.cardPwd = cardPwd;
+	}
+
+}
