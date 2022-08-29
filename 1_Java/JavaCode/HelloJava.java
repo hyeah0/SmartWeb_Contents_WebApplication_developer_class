ad34c4c7758de4d1fcff5c180cb24f504c071b8f
hye <hye0ah0@gmail.com>
2022-08-29 15:27:18 +0900

    Create HelloJava.java

---
+++ 1_Java/JavaCode/HelloJava.java
@@ -0,0 +1,14 @@
+/*
+    비주얼 스튜디오 코드로 자바 파일 실행 
+    0. 터미널 실행 - 원하는 파일 오른쪽 클릭 -> open in integrated terminal
+    1. javac <file name>.java    // 클래스파일 생성됨 
+    2. java <file name>          // 파일 실행됨
+
+    파일 수정시 javac 파일이름.java 작성후 
+    java 파일이름 작성하기
+*/
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
