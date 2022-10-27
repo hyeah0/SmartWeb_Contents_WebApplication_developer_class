ccbf9166225ca7e735a1bd8aa25e29b566287e5b
hye <hye0ah0@gmail.com>
2022-10-27 09:41:12 +0900

    Create DetailBookAction.java

---
+++ 5_JSP_Project_서점/src/com/user/action/DetailBookAction.java
@@ -0,0 +1,24 @@
+package com.user.action;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.book.controller.Action;
+import com.book.controller.ActionForward;
+
+public class DetailBookAction implements Action {
+
+    @Override
+    public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        // get방식으로 받아온 book num
+        int book_num = Integer.valueOf(request.getParameter("book_num")); 
+        System.out.println("book_num >>> " + book_num);
+        
+        
+        return null;
+    }
+
+}
