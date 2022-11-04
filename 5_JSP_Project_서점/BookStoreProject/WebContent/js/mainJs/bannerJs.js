ff08d20581ea822ea2957d355b5cbf6bae0ae007
hye <hye0ah0@gmail.com>
2022-11-04 09:22:28 +0900

    Create bannerJs.js

---
+++ 5_JSP_Project_서점/BookStoreProject/WebContent/js/mainJs/bannerJs.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+ console.log('js연결 완료!')
+
+// 슬라이더 배열로 가져오기
+const slides = document.querySelectorAll(".slide")
+
+// 왼쪽, 오른쪽 버튼 가져오기
+const leftBtn = document.querySelector('.banner_arrow_left')
+const rightBtn = document.querySelector('.banner_arrow_right')
+
+// 실행된 슬라이더
+let activeSlide = 0;
+
+// 함수
+// 현재 실행중인 슬라이드에 active 클래스 추기
+function setActiveSlide(){
+    slides.forEach(slide=>{
+        slide.classList.remove('active')
+    })
+
+    slides[activeSlide].classList.add('active')
+}
+
+// 버튼 클릭시 슬라이드 움직임
+leftBtn.addEventListener('click',()=>{
+    activeSlide--
+
+    if(activeSlide<0){
+        activeSlide = slides.length-1;
+    }
+    setActiveSlide();
+})
+
+rightBtn.addEventListener('click',()=>{
+    activeSlide ++
+
+    if(activeSlide > slides.length-1){
+        activeSlide = 0;
+    }
+    setActiveSlide();
+})
