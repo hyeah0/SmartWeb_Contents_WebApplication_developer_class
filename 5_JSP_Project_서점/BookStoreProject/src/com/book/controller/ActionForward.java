d036c42be1ce497d22c74cfe3eb1ba0ecd377304
hye <hye0ah0@gmail.com>
2022-11-06 18:42:19 +0900

    Update ActionForward.java

--- 5_JSP_Project_서점/BookStoreProject/src/com/book/controller/ActionForward.java
+++ 5_JSP_Project_서점/BookStoreProject/src/com/book/controller/ActionForward.java
@@ -2,20 +2,25 @@ package com.book.controller;
 
 public class ActionForward {
 
-	private boolean isRedirect;
-	private String path;
+	private boolean isRedirect;  
 	
+	private String path;  
+
 	public boolean isRedirect() {
 		return isRedirect;
 	}
+
 	public void setRedirect(boolean isRedirect) {
 		this.isRedirect = isRedirect;
 	}
+
 	public String getPath() {
 		return path;
 	}
+
 	public void setPath(String path) {
 		this.path = path;
 	}
+	
 	
 }
