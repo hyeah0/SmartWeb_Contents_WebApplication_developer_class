d16ff9fd9069b93131d1b4dd9fec0aa43762ab53
hye <hye0ah0@gmail.com>
2022-11-06 18:43:23 +0900

    Update MemberDTO.java

--- 5_JSP_Project_서점/BookStoreProject/src/com/book/model/MemberDTO.java
+++ 5_JSP_Project_서점/BookStoreProject/src/com/book/model/MemberDTO.java
@@ -1,80 +1,78 @@
 package com.book.model;
 
-
 public class MemberDTO {
-    
-    private int mem_num;
-    private String mem_id;
-    private String mem_pwd;
-    private String mem_name;
-    private String mem_phone;
-    private String mem_addr;
-    private String mem_mail;
-    private String mem_card_num;
-    private int mem_car_pwd;
-    private int mem_mileage;
-    
-    
-    public int getMem_num() {
-        return mem_num;
-    }
-    public void setMem_num(int mem_num) {
-        this.mem_num = mem_num;
-    }
-    public String getMem_id() {
-        return mem_id;
-    }
-    public void setMem_id(String mem_id) {
-        this.mem_id = mem_id;
-    }
-    public String getMem_pwd() {
-        return mem_pwd;
-    }
-    public void setMem_pwd(String mem_pwd) {
-        this.mem_pwd = mem_pwd;
-    }
-    public String getMem_name() {
-        return mem_name;
-    }
-    public void setMem_name(String mem_name) {
-        this.mem_name = mem_name;
-    }
-    public String getMem_phone() {
-        return mem_phone;
-    }
-    public void setMem_phone(String mem_phone) {
-        this.mem_phone = mem_phone;
-    }
-    public String getMem_addr() {
-        return mem_addr;
-    }
-    public void setMem_addr(String mem_addr) {
-        this.mem_addr = mem_addr;
-    }
-    public String getMem_mail() {
-        return mem_mail;
-    }
-    public void setMem_mail(String mem_mail) {
-        this.mem_mail = mem_mail;
-    }
-    public String getMem_card_num() {
-        return mem_card_num;
-    }
-    public void setMem_card_num(String mem_card_num) {
-        this.mem_card_num = mem_card_num;
-    }
-    public int getMem_car_pwd() {
-        return mem_car_pwd;
-    }
-    public void setMem_car_pwd(int mem_car_pwd) {
-        this.mem_car_pwd = mem_car_pwd;
-    }
-    public int getMem_mileage() {
-        return mem_mileage;
-    }
-    public void setMem_mileage(int mem_mileage) {
-        this.mem_mileage = mem_mileage;
-    }
-    
-    
+	private int mem_num;
+	private String mem_id;
+	private String mem_pwd;
+	private String mem_name;
+	private String mem_phone;
+	private String mem_addr;
+	private String mem_mail;
+	private String mem_card_num;
+	private int mem_card_pwd;
+	private int mem_mileage;
+	
+	
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getMem_pwd() {
+		return mem_pwd;
+	}
+	public void setMem_pwd(String mem_pwd) {
+		this.mem_pwd = mem_pwd;
+	}
+	public String getMem_name() {
+		return mem_name;
+	}
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	public String getMem_addr() {
+		return mem_addr;
+	}
+	public void setMem_addr(String mem_addr) {
+		this.mem_addr = mem_addr;
+	}
+	public String getMem_mail() {
+		return mem_mail;
+	}
+	public void setMem_mail(String mem_mail) {
+		this.mem_mail = mem_mail;
+	}
+	public String getMem_card_num() {
+		return mem_card_num;
+	}
+	public void setMem_card_num(String mem_card_num) {
+		this.mem_card_num = mem_card_num;
+	}
+	public int getMem_card_pwd() {
+		return mem_card_pwd;
+	}
+	public void setMem_card_pwd(int mem_card_pwd) {
+		this.mem_card_pwd = mem_card_pwd;
+	}
+	public int getMem_mileage() {
+		return mem_mileage;
+	}
+	public void setMem_mileage(int mem_mileage) {
+		this.mem_mileage = mem_mileage;
+	}
+	public String getMem_phone() {
+		return mem_phone;
+	}
+	public void setMem_phone(String mem_phone) {
+		this.mem_phone = mem_phone;
+	}
+	
+	
 }
