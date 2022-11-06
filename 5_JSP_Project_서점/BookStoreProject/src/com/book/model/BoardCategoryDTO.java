17867c9676221f4ffcbe5de668aaabcb0665082a
hye <hye0ah0@gmail.com>
2022-11-06 18:43:07 +0900

    Create BoardCategoryDTO.java

---
+++ 5_JSP_Project_서점/BookStoreProject/src/com/book/model/BoardCategoryDTO.java
@@ -0,0 +1,21 @@
+package com.book.model;
+
+public class BoardCategoryDTO {
+	private String board_cgy_num;
+	private String board_cgy_name;
+	
+	public String getBoard_cgy_num() {
+		return board_cgy_num;
+	}
+	public void setBoard_cgy_num(String board_cgy_num) {
+		this.board_cgy_num = board_cgy_num;
+	}
+	public String getBoard_cgy_name() {
+		return board_cgy_name;
+	}
+	public void setBoard_cgy_name(String board_cgy_name) {
+		this.board_cgy_name = board_cgy_name;
+	}
+	
+	
+}
