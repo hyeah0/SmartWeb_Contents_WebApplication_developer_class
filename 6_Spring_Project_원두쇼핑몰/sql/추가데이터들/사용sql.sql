254c9bf156b0337f52f3a7faaf70972674760237
hye <hye0ah0@gmail.com>
2022-12-06 00:34:39 +0900

    Update 사용sql.sql

--- 6_Spring_Project_원두쇼핑몰/sql/추가데이터들/사용sql.sql
+++ 6_Spring_Project_원두쇼핑몰/sql/추가데이터들/사용sql.sql
@@ -14,6 +14,27 @@ select cart_num
 ------ 상품이 장바구니에 없을경우 0 값
 select nvl(max(cart_num),0) as cart_num
      , nvl(max(cart_cnt),0) as cart_cnt
+     , nvl(max(cart_weight),0) as cart_weight
+     , nvl(max(cart_grind),0) as cart_grind 
   from coffee_cart
  where member_num = 1
-   and beans_num = 2;
+   and beans_num = 10
+   and cart_grind = 0;
+   
+--- 장바구니 가져오기
+select ca.cart_num
+     , ca.beans_num
+     , ca.cart_cnt
+     , ca.cart_weight
+     , ca.cart_grind
+     , be.beans_name
+     , be.beans_price
+     , beans_taste
+     , beans_img
+     , beans_url
+  from coffee_cart ca
+  join coffee_beans be
+    on ca.beans_num = be.beans_num
+ where member_num = 1
+ order by cart_num desc; 
+  
