5748383cbd6993b3fd43e487c4ee1a2e95be04e4
hye <hye0ah0@gmail.com>
2022-12-13 12:29:06 +0900

    페이지

--- 6_Spring_Project_원두쇼핑몰/HTML/js/2안js/cart/order.js
+++ 6_Spring_Project_원두쇼핑몰/HTML/js/2안js/cart/order.js
@@ -2,17 +2,17 @@ console.log('order.js')
 
 // 상세보기 
 // 변수
-let offCanvas = document.querySelector('.offCanvas')
+let slider = document.querySelector('.slider')
 let summaryOrder = document.querySelector('.summary_order')
  
 // 버튼 클릭시 주문 상세보기 화면 나오는 함수
 function showDetail(){
-    offCanvas.classList.add('show')
+    slider.classList.add('show')
     summaryOrder.classList.add('hidden')
 }
 
 // 버튼 클릭시 주문 상세보기 들어가는 함수
 function hideDetail(){
-    offCanvas.classList.remove('show')
+    slider.classList.remove('show')
     summaryOrder.classList.remove('hidden')
 }
