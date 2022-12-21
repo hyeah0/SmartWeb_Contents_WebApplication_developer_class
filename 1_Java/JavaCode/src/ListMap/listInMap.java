61aef28537a346847f7c9df309ac0a0511d410bc
hye <hye0ah0@gmail.com>
2022-12-21 17:48:56 +0900

    Update listInMap.java

--- 1_Java/JavaCode/src/ListMap/listInMap.java
+++ 1_Java/JavaCode/src/ListMap/listInMap.java
@@ -1,5 +1,10 @@
 package ListMap;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class listInMap {
 
 	public static void main(String[] args) {
@@ -14,6 +19,22 @@ public class listInMap {
 		 * 1. list 전체 출력
 		 * 
 		 */
+		
+		// 1. list 준비
+		List<Map<String, Object>> list = new ArrayList<Map<String,Object>>();
+		
+		// 2. map 준비
+		Map<String, Object> studentMap = new HashMap<String, Object>();
+		
+		studentMap.put("name", "김아무개");
+		studentMap.put("age", "20");
+		studentMap.put("major", "경영학과");
+		studentMap.put("email", "aaa@naver.com");
+		studentMap.put("phone", "010-1234-5678");
+		
+		// 3. list에 map 추가
+		list.add(studentMap);
+		
 	}
 
 }
