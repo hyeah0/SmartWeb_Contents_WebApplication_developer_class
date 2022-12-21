fbc6baf3fe28eeecc8f1e00330beaec568832a47
hye <hye0ah0@gmail.com>
2022-12-21 17:44:52 +0900

    list 안에 map

---
+++ 1_Java/JavaCode/src/ListMap/listInMap.java
@@ -0,0 +1,19 @@
+package ListMap;
+
+public class listInMap {
+
+	public static void main(String[] args) {
+		// list 안에 map
+		
+		/*
+		 * 1. list 생성 
+		 * 2. map 생성 및 정보 저장
+		 * 3. list에 map 추가
+		 * 
+		 * 출력
+		 * 1. list 전체 출력
+		 * 
+		 */
+	}
+
+}
